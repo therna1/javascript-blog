273b611a391f78d1e9727ea681b824342cbe03ca
Oliwia Gabrych <marsmusic@op.pl>
2020-11-25 16:20:28 +0100

    Remove 'active' class from articles

--- js/script.js
+++ js/script.js
@@ -6,9 +6,20 @@ function titleClickHandler(event){
 
   /* remove class 'active' from all article links  */
 
+  const activeLinks = document.querySelectorAll ('.titles a.active');
+  for(let activeLink of activeLinks) {
+      activeLink.classList.remove ('active')
+  }
+
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('article');
+  for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove('active')
+  }
+
+
 
   /* get 'href' attribute from the clicked link */
 
