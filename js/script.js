6221d8ad91102e876f559df5bd0f8000940df211
Oliwia Gabrych <marsmusic@op.pl>
2020-11-30 22:09:24 +0100

    Add links for articles

--- js/script.js
+++ js/script.js
@@ -3,45 +3,53 @@
 function titleClickHandler(event) {
     event.preventDefault();
     const clickedElement = this;
-    console.log('link was clicked');
-
-
-    /* remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active')
     }
 
-    /* add class 'active' to the clicked link */
-
     clickedElement.classList.add('active')
-
-
-    /* remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('article');
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active')
     }
 
-    /* get 'href' attribute from the clicked link */
-
     const articleSelector = clickedElement.getAttribute('href');
-    console.log(articleSelector)
-
-    /* find the correct article using the selector (value of 'href' attribute) */
 
     const targetArticle = document.querySelector(articleSelector)
-    console.log(targetArticle)
-
-    /* add class 'active' to the correct article */
+  
     targetArticle.classList.add('active')
 }
 
-const links = document.querySelectorAll('.titles a');
 
 
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
+const optArticleSelector = '.post', 
+  optTitleSelector = '.post-title',
+  optTitleListSelector = document.querySelector('.titles');
+
+function generateTitleLinks(){
+
+const titleList = optTitleListSelector;
+titleList.innerHTML='';
+
+const articles = document.querySelectorAll('article')
+
+for (let article of articles) {
+const articleId = article.getAttribute('id');
+
+const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+
+optTitleListSelector.insertAdjacentHTML('beforeend', linkHTML);
 }
+
+const links = document.querySelectorAll('.titles a');
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler); 
+}
+};
+
+generateTitleLinks();
